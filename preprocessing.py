b4bfb1a6b202cdebe34eea853b879dbd6ad38e70
Gabriel Ukstin Talasso <75808460+GabrielTalasso@users.noreply.github.com>
2023-07-07 08:15:46 -0300

    preprocessing

---
+++ preprocessing.py
@@ -0,0 +1,17 @@
+#este arquivo e responsavel por conter a funcao de preprocessamento de dados financeiros
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def preprocessing(data):
+
+    #tratamento de NA
+
+    #tratamento de datas
+
+    #normalizacao 
+
+    #tratamento de categoricas
+
+    return data
