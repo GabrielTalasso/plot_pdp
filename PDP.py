582c32b073d06040db01183cc54a3195932ad51c
Gabriel Ukstin Talasso <75808460+GabrielTalasso@users.noreply.github.com>
2023-07-07 10:10:43 -0300

    pdp

---
+++ PDP.py
@@ -0,0 +1,18 @@
+
+
+class PDP():
+
+    def __init__(self):
+        pass
+
+    def pre_processing(self):
+        #faz todo processamento e ratamento dos dados
+        pass
+
+    def calcule_pdp(self):
+        #faz o calculo e retorna os valores calculados do pdp
+        pass
+
+
+    def plot_pdp(selp):
+        #plota, usando bibli existentes, o grafico de pdp (ainda nao interativo)
--- preprocessing.py
+++ preprocessing.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+path = './data/cre '
+
 def preprocessing(data):
 
     #tratamento de NA
