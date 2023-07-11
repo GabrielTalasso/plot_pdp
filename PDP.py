1c97d393695a0fddb92176c36a9de035621fc786
GabrielTalasso <gukstin@gmail.com>
2023-07-11 09:40:04 -0300

    modified:   PDP.py 	modified:   __pycache__/preprocessing.cpython-38.pyc 	new file:   data/german_raw.csv 	modified:   preprocessing.py 	modified:   tests.ipynb

--- PDP.py
+++ PDP.py
@@ -1,12 +1,18 @@
-
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn.inspection import PartialDependenceDisplay
 
 class PDP():
 
     def __init__(self):
         pass
 
+    def load_dataset(self, path):
+        pass
+
     def pre_processing(self):
-        #faz todo processamento e ratamento dos dados
+        #faz todo processamento e tratamento dos dados
         pass
 
     def calcule_pdp(self):
@@ -17,3 +23,5 @@ class PDP():
     def plot_pdp(selp):
         #plota, usando bibli existentes, o grafico de pdp (ainda nao interativo)
         pass
+
+    
--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,37 @@
 #este arquivo e responsavel por conter a funcao de preprocessamento de dados financeiros
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from datetime import datetime
-
-path = './data/german_credit_data.csv'
 
 def preprocessing(data, NA = 'drop',
                   to_date = 'none', date_format = 'none',
                   normalization = False, norm_type = 'norm',
                   dummies = 'none'):
+    
+    """
+    Perform preprocessing on a dataset.
+
+    Args:
+        data (object): The dataset to be preprocessed.
+        NA (str, optional): Determines how to handle missing values (NA). Defaults to 'drop'.
+            - 'drop': Remove rows containing missing values.
+            - 'mean_mode': Replace missing values in numeric columns with the mean, and in categorical columns with the mode.
+        to_date (str, optional): The name of the column containing date data. Defaults to 'none'.
+            If different from 'none', the column will be converted to a date format.
+        date_format (str, optional): The format of the date in the 'to_date' column. Defaults to 'none'.
+            If 'none', the default format will be used. Otherwise, a valid date format must be specified.
+        normalization (bool, optional): Determines whether to perform normalization on numeric data. Defaults to False.
+            If True, numeric data will be normalized; otherwise, no processing will be done.
+        norm_type (str, optional): The type of normalization to apply. Defaults to 'norm'.
+            - 'norm': Standard normalization using mean and standard deviation.
+            - 'min_max': Min-max scaling using the minimum and maximum values of the data.
+        dummies (str or list, optional): Determines how to handle categorical columns. Defaults to 'none'.
+            - 'none': No transformation will be applied to categorical columns.
+            - 'all': All categorical columns will be converted to dummy variables.
+            - List of specific categorical column names to be converted to dummy variables.
+
+    Returns:
+        object: The preprocessed dataset.
+    """
 
     data_num = data.select_dtypes(include=np.number) #only numerical columns
     data_cat = data.drop(data_num.columns, axis = 1) #only categorical columns
@@ -41,9 +62,9 @@ def preprocessing(data, NA = 'drop',
     if to_date != 'none':
 
         if date_format == 'none':
-            data[to_date] = pd.to_datetime(to_date)
+            data[to_date] = pd.to_datetime(data[to_date])
         else:
-            data[to_date] = pd.to_datetime(to_date, format = date_format)
+            data[to_date] = pd.to_datetime(data[to_date], format = date_format)
 
     #normalizacao 
 
@@ -56,13 +77,13 @@ def preprocessing(data, NA = 'drop',
             data_num = (data_num - data_num.min())/(data_num.max() - data_num.min())
             
 
-    #tratamento de categoricas
+    #categorical columns
     if dummies != 'none':
 
         if dummies == 'all':
             dummies = data_cat.columns
 
-        data_cat = pd.concat([data_cat, pd.get_dummies(data[dummies])], axis = 1)
+        data_cat = pd.concat([data_cat, pd.get_dummies(data[dummies])*1], axis = 1)
         data_cat = data_cat.drop(dummies, axis = 1)
 
 
