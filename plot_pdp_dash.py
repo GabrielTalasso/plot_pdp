795dde7ce23434337a5eed896903a834251b1629
GabrielTalasso <gukstin@gmail.com>
2023-08-07 09:39:30 -0300

    modified:   __pycache__/plot_pdp_dash.cpython-38.pyc 	modified:   example_pdp_german.ipynb 	modified:   plot_pdp_dash.py

--- plot_pdp_dash.py
+++ plot_pdp_dash.py
@@ -4,7 +4,7 @@ import pandas as pd
 from jupyter_dash import JupyterDash
 
 def plot_pdp_dash(data, pdp_results, feature):
-    pdp_df = pd.DataFrame({'grid_values': pdp_results['values'][0], 'average': pdp_results['average'][0]})
+    pdp_df = pd.DataFrame({'x': pdp_results['values'][0], 'y': pdp_results['average'][0]})
     pdp_dict = pdp_df.to_dict(orient='records')
 
     app = JupyterDash(__name__)
@@ -14,8 +14,8 @@ def plot_pdp_dash(data, pdp_results, feature):
             id='linear-hist',
             data=pdp_dict,
             histData=data.to_dict(orient='records'),
-            x_axis='grid_values',
-            y_axis='average',
+            x_axis='x',
+            y_axis='y',
             histValue=feature
         ),
     ])
